c751484237c76d08c84563bbcd64f8025f5d6032
DaikiHiguchi <daikiit27@gmail.com>
2021-04-04 22:08:52 +0900

    Initial Commit

---
+++ BASICS_Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BASICS_Realm
+//
+//  Created by 樋口大樹 on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
