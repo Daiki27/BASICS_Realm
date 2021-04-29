f1b1bdbda59e3c831110895a0fe2787194d300b4
DaikiHiguchi <daikiit27@gmail.com>
2021-04-29 17:27:33 +0900

    initial commit

--- BASICS_Realm/ViewController.swift
+++ BASICS_Realm/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate{
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Realmのインスタンスを取得
+        let realm = try! Realm()
+        //全件取得する.
+        let animal = realm.objects(Animal.self)
+        for animalData in animal{
+            label.text = animalData.name
+            print(animalData)
+        }
+        //入力してEnter押したときのために.
+        textField.delegate = self
+        
     }
-
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //入力してEnter押すとキーボードが閉じる.
+        label.text = textField.text
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+        //データベースにデータを保存する.
+        let animal = Animal()
+        animal.name = textField.text
+        label.text = animal.name
+        
+        let realm = try! Realm()
+        try! realm.write {
+            print("Success!")
+            realm.add(animal)
+        }
+    }
 }
-
---
+++ BASICS_Realm/model.swift
@@ -0,0 +1,13 @@
+//
+//  model.swift
+//  BASICS_Realm
+//
+//  Created by 樋口大樹 on 2021/04/04.
+//
+
+import UIKit
+import RealmSwift
+
+class Animal: Object{
+   @objc dynamic var name: String?
+}
